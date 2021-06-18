5dd6cfb5bef0d512a173aa996da0991fe0927649
Jim <jimascencio80@gmail.com>
2021-06-17 22:35:01 -0600

    Header con sesiones añadido

--- Sistema de compra y venta/vistas/header.php
+++ Sistema de compra y venta/vistas/header.php
@@ -1,13 +1,17 @@
+<?php
+if (strlen(session_id()) < 1) 
+  session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Lacteos la Vaca Lola</title>
+    <title>Venta de Lacteos La vaca Lola</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
-  <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="../public/css/font-awesome.css">
     <!-- Theme style -->
@@ -15,15 +19,16 @@
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="../public/css/_all-skins.min.css">
-    <link rel="apple-touch-icon" href="../public/img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" href="../public/img/vaca.png">
     <link rel="shortcut icon" href="../public/img/vaca.ico">
 
     <!-- DATATABLES -->
     <link rel="stylesheet" type="text/css" href="../public/datatables/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="../public/datatables/buttons.dataTables.min.css">
-    <link rel="stylesheet" href="../public/datatables/responsive.dataTables.min.css">
+    <link href="../public/datatables/buttons.dataTables.min.css" rel="stylesheet"/>
+    <link href="../public/datatables/responsive.dataTables.min.css" rel="stylesheet"/>
 
-    <link rel="stylesheet" href="../public/css/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/bootstrap-select.min.css">
+
   </head>
   <body class="hold-transition skin-green-light sidebar-mini">
     <div class="wrapper">
@@ -31,11 +36,11 @@
       <header class="main-header">
 
         <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <a class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>Lacteos</b>Ventas</span>
+          <span class="logo-mini"><b>La Vaca</b>Lola</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Lacteos la Vaca Lola</b></span>
+          <span class="logo-lg"><b>La vaca Lola</b></span>
         </a>
 
         <!-- Header Navbar: style can be found in header.less -->
@@ -52,16 +57,16 @@
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="../public/img/vaca.png" class="user-image" alt="User Image">
-                  <span class="hidden-xs">Lacteos</span>
+                  <img src="../files/usuarios/<?php echo $_SESSION['imagen']; ?>" class="user-image" alt="User Image"><!-- muestra la imagen que esta ligada al usuario -->
+                  <span class="hidden-xs"><?php echo $_SESSION['nombre']; ?></span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
-                    <img src="../public/img/vaca.png" class="img-circle" alt="User Image">
+                    <img src="../files/usuarios/<?php echo $_SESSION['imagen']; ?>" class="img-circle" alt="User Image"><!-- muestra la imagen que esta ligada al usuario -->
                     <p>
-                      La vaca Lola
-                      <small>Lacteos</small>
+                     <small>Venta de Lacteos</small>
+                     La vaca Lola
                     </p>
                   </li>
                   
@@ -69,7 +74,7 @@
                   <li class="user-footer">
                     
                     <div class="pull-right">
-                      <a href="#" class="btn btn-default btn-flat">Cerrar</a>
+                      <a href="../ajax/usuario.php?op=salir" class="btn btn-default btn-flat">Cerrar</a>
                     </div>
                   </li>
                 </ul>
@@ -87,89 +92,129 @@
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
             <li class="header"></li>
-            <li>
-              <a href="#">
+            <?php 
+            if ($_SESSION['escritorio']==1)
+            {
+              echo '<li id="mEscritorio">
+              <a href="escritorio.php">
                 <i class="fa fa-tasks"></i> <span>Escritorio</span>
               </a>
-            </li>            
-            <li class="treeview">
+            </li>';
+            }
+            ?>
+
+            <?php 
+            if ($_SESSION['almacen']==1)
+            {
+              echo '<li id="mAlmacen" class="treeview">
               <a href="#">
                 <i class="fa fa-laptop"></i>
                 <span>Almacén</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="articulo.php"><i class="fa fa-circle-o"></i> Artículos</a></li>
-                <li><a href="categoria.php"><i class="fa fa-circle-o"></i> Categorías</a></li>
+                <li id="lArticulos"><a href="articulo.php"><i class="fa fa-circle-o"></i> Artículos</a></li>
+                <li id="lCategorias"><a href="categoria.php"><i class="fa fa-circle-o"></i> Categorías</a></li>
               </ul>
-            </li>
-            
-            <li class="treeview">
+            </li>';
+            }
+            ?>
+
+            <?php 
+            if ($_SESSION['compras']==1)
+            {
+              echo '<li id="mCompras" class="treeview">
               <a href="#">
                 <i class="fa fa-th"></i>
                 <span>Compras</span>
                  <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="ingreso.php"><i class="fa fa-circle-o"></i> Ingresos</a></li>
-                <li><a href="proveedor.php"><i class="fa fa-circle-o"></i> Proveedores</a></li>
+                <li id="lIngresos"><a href="ingreso.php"><i class="fa fa-circle-o"></i> Ingresos</a></li>
+                <li id="lProveedores"><a href="proveedor.php"><i class="fa fa-circle-o"></i> Proveedores</a></li>
               </ul>
-            </li>
-            <li class="treeview">
+            </li>';
+            }
+            ?>
+
+            <?php 
+            if ($_SESSION['ventas']==1)
+            {
+              echo '<li id="mVentas" class="treeview">
               <a href="#">
                 <i class="fa fa-shopping-cart"></i>
                 <span>Ventas</span>
                  <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="venta.php"><i class="fa fa-circle-o"></i> Ventas</a></li>
-                <li><a href="cliente.php"><i class="fa fa-circle-o"></i> Clientes</a></li>
+                <li id="lVentas"><a href="venta.php"><i class="fa fa-circle-o"></i> Ventas</a></li>
+                <li id="lClientes"><a href="cliente.php"><i class="fa fa-circle-o"></i> Clientes</a></li>
               </ul>
-            </li>                       
-            <li class="treeview">
+            </li>';
+            }
+            ?>
+                        
+            <?php 
+            if ($_SESSION['acceso']==1)
+            {
+              echo '<li id="mAcceso" class="treeview">
               <a href="#">
                 <i class="fa fa-folder"></i> <span>Acceso</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="usuario.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
-                <li><a href="permiso.php"><i class="fa fa-circle-o"></i> Permisos</a></li>
+                <li id="lUsuarios"><a href="usuario.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
+                <li id="lPermisos"><a href="permiso.php"><i class="fa fa-circle-o"></i> Permisos</a></li>
                 
               </ul>
-            </li>
-            <li class="treeview">
+            </li>';
+            }
+            ?>
+
+            <?php 
+            if ($_SESSION['consultac']==1)
+            {
+              echo '<li id="mConsultaC" class="treeview">
               <a href="#">
                 <i class="fa fa-bar-chart"></i> <span>Consulta Compras</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="consultacompras.php"><i class="fa fa-circle-o"></i> Consulta Compras</a></li>                
+                <li id="lConsulasC"><a href="comprasfecha.php"><i class="fa fa-circle-o"></i> Consulta Compras</a></li>                
               </ul>
-            </li>
-            <li class="treeview">
+            </li>';
+            }
+            ?>
+
+             <?php 
+            if ($_SESSION['consultav']==1)
+            {
+              echo '<li id="mConsultaV" class="treeview">
               <a href="#">
                 <i class="fa fa-bar-chart"></i> <span>Consulta Ventas</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="consultaventas.php"><i class="fa fa-circle-o"></i> Consulta Ventas</a></li>                
+                <li id="lConsulasV"><a href="ventasfechacliente.php"><i class="fa fa-circle-o"></i> Consulta Ventas</a></li>                
               </ul>
-            </li>
-            <li>
-              <a href="#">
+            </li>';
+            }
+            ?>
+
+            <!--<li>Etiquetas de ayuda
+              <a href="ayuda.php">
                 <i class="fa fa-plus-square"></i> <span>Ayuda</span>
                 <small class="label pull-right bg-red">PDF</small>
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="acerca.php">
                 <i class="fa fa-info-circle"></i> <span>Acerca De...</span>
-                
+                <small class="label pull-right bg-yellow">IT</small>
               </a>
-            </li>
+            </li>-->
                         
           </ul>
         </section>
         <!-- /.sidebar -->
       </aside>
-
